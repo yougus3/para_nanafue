909b868f347e1c4895344f7e629e4125b7140d00
Gustavo Miranda Silva <gustavodevmiranda@gmail.com>
2024-02-06 05:29:06 -0600

    MainAcitivity con fondo rojo degradado, dos botones: positivo y negativo.
    AlertDialog con layout personalizado
    
    Nana ilustración

--- app/src/main/java/com/pixelarc/parananafue/MainActivity.kt
+++ app/src/main/java/com/pixelarc/parananafue/MainActivity.kt
@@ -2,10 +2,23 @@ package com.pixelarc.parananafue
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.pixelarc.parananafue.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnNo.setOnClickListener {
+            val alert = AlertDialog.Builder(this)
+            alert.setTitle("Mensaje")
+            //alert.setPositiveButton("Ta' güeno", null)
+            alert.setView(R.layout.negative_response)
+            alert.show()
+        }
     }
 }
